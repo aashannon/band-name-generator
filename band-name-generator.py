137be0d7e9e353a470d548b18a0fe9144dd9f3e0
Aaron Shannon <ayren.shannon@me.com>
2024-12-06 23:53:44 -0800

    Simple band name generator. It was fun

---
+++ band-name-generator.py
@@ -0,0 +1,7 @@
+# This will take the name of the city you grew up in and the name of your pet and generate a band name
+
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+
+print(f"Your band name could be {city} {pet}")
